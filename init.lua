81ab0e6edc081495e66232055bca4dd1dd1478a7
Max <max@maxos.me>
2019-05-25 08:31:27 +0800

    udpate

--- init.lua
+++ init.lua
@@ -50,6 +50,30 @@ hs.hotkey.bind({"alt", "ctrl"}, "Down", function()
     win:setFrame(f)
 end)
 
+hs.hotkey.bind({"alt", "ctrl"}, "F", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+    local screen = win:screen()
+    local max = screen:frame()
+
+    f.x = max.x
+    f.y = max.y
+    f.w = max.w
+    f.h = max.h
+    win:setFrame(f)
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "F", function()
+    local win = hs.window.focusedWindow()
+    win:toggleFullScreen()
+    -- if win:isFullScreen() then
+    -- 	win:setFullScreen(false)
+    -- else
+    -- 	win:setFullScreen(true)
+    -- end
+
+end)
+
 hs.hotkey.bind({"shift", "alt", "ctrl"}, "Left", function()
     local win = hs.window.focusedWindow()
     local f = win:frame()
@@ -154,7 +178,42 @@ hs.hotkey.bind({"shift", "cmd", "alt", "ctrl"}, "Right", function()
     win:setFrame(f)
 end)
 
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+    hs.window.switcher.next()
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "F", function()
+	local win = hs.window.focusedWindow()
+	local title = win:title()
+	hs.notify.new({title="Get Focused Title", informativeText=title, hasActionButton=false}):send()
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "T", function()
+    local win = hs.window:windowsToEast()
+    win:focus()
+    hs.notify.new({title="Hammerspoon", informativeText=win, hasActionButton=false}):send()
+end)
+
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "R", function()
   hs.reload()
   hs.notify.new({title="Hammerspoon", informativeText="Config reloaded.", hasActionButton=false}):send()
 end)
+
+
+caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+    if state then
+        caffeine:setTitle("AWAKE")
+    else
+        caffeine:setTitle("SLEEPY")
+    end
+end
+
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
